2b8f7b117e835694b3b7ab6cc904f15ed3c835b7
ayakimchuk <ayakimchuk@platinumbank.com.ua>
2016-12-22 16:10:40 +0200

    changed result to string logic

--- src/main/java/ua/com/platinumbank/util/JSONUtil.java
+++ src/main/java/ua/com/platinumbank/util/JSONUtil.java
@@ -9,24 +9,21 @@ import ua.com.platinumbank.model.Address;
 // TODO add javadoc
 public class JSONUtil {
 
-	public static String addressToJSONString(Address address) {
+    public static String addressToJSONString(Address address) {
 
-		String jsonAddress = null;
+        String jsonAddress = null;
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
 
-		try {
-			jsonAddress = objectWriter.writeValueAsString(address);
-		} catch (JsonProcessingException e) {
-			// TODO replace with logging
-			e.printStackTrace();
-		}
+        try {
+            jsonAddress = objectWriter.writeValueAsString(address);
+        } catch (JsonProcessingException e) {
+            // TODO replace with logging
+            e.printStackTrace();
+        }
 
-		// XXX for testing
-		System.out.println(jsonAddress);
-
-		return jsonAddress;
-	}
+        return jsonAddress;
+    }
 
 }
