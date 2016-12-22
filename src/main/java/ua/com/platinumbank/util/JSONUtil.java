d9e8e7a1aecbfb8cfa4a05db39614bc805fb83d6
ayakimchuk <ayakimchuk@platinumbank.com.ua>
2016-12-22 17:35:08 +0200

    added method to parse json request into address object

--- src/main/java/ua/com/platinumbank/util/JSONUtil.java
+++ src/main/java/ua/com/platinumbank/util/JSONUtil.java
@@ -1,5 +1,7 @@
 package ua.com.platinumbank.util;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -24,6 +26,23 @@ public class JSONUtil {
         }
 
         return jsonAddress;
+    }
+
+    // TODO add javadoc
+    public static Address parseJsonString(String jsonString) {
+
+        Address addressRequest = null;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            addressRequest = objectMapper.readValue(jsonString, Address.class);
+        } catch (IOException e) {
+            // TODO replace with logging
+            e.printStackTrace();
+        }
+
+        return addressRequest;
     }
 
 }
